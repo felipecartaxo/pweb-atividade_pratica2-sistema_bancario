6da93f806a0a112812537287d052bc54ca5b3a1e
unknown <felipecartaxo1@gmail.com>
2024-12-03 09:30:10 -0300

    Implementação dos métodos inserir, remover e editar

--- app/ts/model/cliente-especial.ts
+++ app/ts/model/cliente-especial.ts
@@ -12,11 +12,37 @@ class ClienteEspecial extends Cliente {
         return this.dependentes;
     }
 
-    adicionarDependente(dependente: Cliente) {
+    adicionarDependente(dependente: Cliente): void {
         if (this.cpf === dependente.cpf) {
-            throw new Error("Não é possível cadastrar o titular como dependente")
+            throw new Error("Não é possível cadastrar o titular como dependente");
         }
 
         this.dependentes.push(dependente);
+    }
+
+    removerDependente(cpf: string): void {
+        // Retorna o dependente cujo cpf é igual ao que foi passado como argumento do método
+        const dependenteARemover: Cliente = this.dependentes.find(
+            dependente => dependente.cpf === cpf
+        );
+
+        if (dependenteARemover) {
+            const index = this.dependentes.indexOf(dependenteARemover);
+            this.dependentes.splice(index, 1);
+        }
+    }
+
+    editarCpfDependente(cpfAntigo: string, cpfNovo: string): void {
+        const dependente = this.dependentes.find(
+            dependente => dependente.cpf === cpfAntigo
+        );
+
+        // Atualizando o cpf
+        if (dependente) {
+            dependente.cpf = cpfNovo;
+        }
+        else {
+            throw new Error("Dependente não encontrado");
+        }
     }
 }
